78995d6e851655cd0fa4284a05540a57005fb31e
aguileracamilo <82463563+aguileracamilo@users.noreply.github.com>
2023-04-02 14:25:54 -0500

    Creación y configuracion del proyecto react, y creación logipage

---
+++ src/Login.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./css/Login.css";
+import SignIn from './components/SignIn'
+import logo from './assets/uniquindio-logo.png'
+
+function Login() {
+  return (
+    <>
+      <div className="login-area">
+        <img src={logo} alt="Logo Uniquindio"/>
+        <SignIn/>
+      </div>
+      <div className="background-login"></div>
+    </>
+  );
+}
+
+export default Login;
---
+++ src/components/SignIn.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function SignIn() {
+  return (
+   <form>
+    <h3>Inicio de Sesión</h3>
+    <input/>
+    <input/>
+    <button>Ingresar</button>
+   </form>
+  )
+}
+
+export default SignIn
